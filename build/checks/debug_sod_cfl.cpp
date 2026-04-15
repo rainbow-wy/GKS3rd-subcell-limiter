09e7f5a8a4685150d218d39db9f3cbedcab9d622
rainbow-wy <498455753@qq.com>
2026-04-15 17:43:33 +0800

    remove build artifacts

--- build/checks/debug_sod_cfl.cpp
+++
@@ -1,9 +0,0 @@
-#include "riemann_problem.h"
-#include <iostream>
-#include <cmath>
-int main(){
-    K=4; Gamma=1.4; R_gas=1.0; tau_type=Euler; c1_euler=0.1; c2_euler=1.0;
-    flux_function=GKS; gks1dsolver=gks3rd; cellreconstruction=WENO5_AO; reconstruction_variable=characteristic; wenotype=wenoz; g0reconstruction=Center_GKS3rd; timecoe_list=S1O3;
-    Block1d block; block.nodex=200; block.ghost=3; block.nx=block.nodex+2*block.ghost; block.nodex_begin=block.ghost; block.nodex_end=block.nodex+block.ghost-1; block.left=0.0; block.right=1.0; block.dx=(block.right-block.left)/block.nodex; block.CFL=0.1; Initial_stages(block);
-    Fluid1d* fluids = new Fluid1d[block.nx]; Interface1d* interfaces = new Interface1d[block.nx+1]; Flux1d** fluxes=Setflux_array(block); SetUniformMesh(block, fluids, interfaces, fluxes); Fluid1d zone1; zone1.primvar[0]=1.0; zone1.primvar[1]=0.0; zone1.primvar[2]=1.0; Fluid1d zone2; zone2.primvar[0]=0.125; zone2.primvar[1]=0.0; zone2.primvar[2]=0.1; ICfor1dRM(fluids, zone1, zone2, block); Fluid1d* bcvalue = new Fluid1d[2]; block.t=0; block.step=0; bool has_nan=false; while(block.t < 0.2-1e-14){ CopyFluid_new_to_old(fluids, block); block.dt=Get_CFL(block, fluids, 0.2); free_boundary_left(fluids, block, bcvalue[0]); free_boundary_right(fluids, block, bcvalue[1]); Reconstruction_within_cell(interfaces, fluids, block); Reconstruction_forg0(interfaces, fluids, block); Calculate_flux(fluxes, interfaces, block, 0); Update(fluids, fluxes, block, 0); block.t += block.dt; block.step++; for(int i=block.ghost;i<block.nx-block.ghost;i++){ if(!(fluids[i].convar[0]==fluids[i].convar[0])) { has_nan=true; break; } } if(has_nan) break; }
-    std::cout << "step=" << block.step << " t=" << block.t << " has_nan=" << has_nan << std::endl; return 0; }
--- build/checks/gks3rd_driver.cpp
+++
@@ -1,7 +0,0 @@
-#include "gks3rd_demo.h"
-int main()
-{
-    accuracy_sinwave_1d_gks3rd();
-    riemann_problem_1d_gks3rd();
-    return 0;
-}
